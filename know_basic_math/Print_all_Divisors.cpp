fe800f986f7f311f7c7b0cc94f987bd0f041de04
Gopalkrushna sahu <sahugopalkrushna2@gmail.com>
2026-07-14 19:24:56 +0530

    basic math questions

---
+++ know_basic_math/Print_all_Divisors.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void allDivisor(int n){
+    for(int i = 1; i<=n; i++){
+        if(n%i==0) cout<< i << " ";
+    }
+}
+
+int main(){
+
+    int n;
+    cout<< "Enter a number: ";
+    cin>> n;
+
+    allDivisor(n);
+
+    return 0;
+}
