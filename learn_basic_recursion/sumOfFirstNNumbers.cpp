8b38a4ef44bd2a7dc2210d33b55ce5a2d9be8822
Gopalkrushna sahu <sahugopalkrushna2@gmail.com>
2026-07-19 20:25:57 +0530

    basic recursion concepts

--- learn_basic_recursion/sumOfFirstNNumbers.cpp
+++ learn_basic_recursion/sumOfFirstNNumbers.cpp
@@ -1,20 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sumoffirstNnumbers(int n){
-    if (n==1) return 1;
+void sumoffirstNnumbers(int i, int sum){
+    if (i<1){
+        cout<< sum << endl;
+        return;
+    }
 
-    return n + sumoffirstNnumbers(n-1);
+    return sumoffirstNnumbers(i-1, sum+i);
 }
 
 int main(){
 
-    int i = 1, n, sum = 0;
+    int n;
     cout<< "Enter a number: ";
     cin>> n;
 
-    int sumofnums = sumoffirstNnumbers(n);
-    cout<< sumofnums << endl;
+    sumoffirstNnumbers(n, 0);
 
     return 0;
 }
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int sumoffirstNnumbers(int n){
+//     if (n==1) return 1;
+
+//     return n + sumoffirstNnumbers(n-1);
+// }
+
+// int main(){
+
+//     int i = 1, n, sum = 0;
+//     cout<< "Enter a number: ";
+//     cin>> n;
+
+//     int sumofnums = sumoffirstNnumbers(n);
+//     cout<< sumofnums << endl;
+
+//     return 0;
+// }
